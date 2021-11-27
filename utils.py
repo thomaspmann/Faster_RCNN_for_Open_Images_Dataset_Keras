84225a96068d0d307724d06dea45eaaf174fb8ed
Thomas Mann <thomasmann@Thomass-MacBook-Pro.local>
2021-11-27 11:00:30 +0100

    First changes

---
+++ utils.py
@@ -0,0 +1,127 @@
+import sys
+import cv2
+
+# Parser for the annotation file
+def get_data(input_path):
+	"""Parse the data from annotation file
+	
+	Args:
+		input_path: annotation file path
+
+	Returns:
+		all_data: list(filepath, width, height, list(bboxes))
+		classes_count: dict{key:class_name, value:count_num} 
+			e.g. {'Car': 2383, 'Mobile phone': 1108, 'Person': 3745}
+		class_mapping: dict{key:class_name, value: idx}
+			e.g. {'Car': 0, 'Mobile phone': 1, 'Person': 2}
+	"""
+	found_bg = False
+	all_imgs = {}
+	classes_count = {}
+	class_mapping = {}
+
+	visualise = True
+
+	i = 1
+	with open(input_path,'r') as f:
+
+		print('Parsing annotation files')
+
+		for line in f:
+
+			# Print process
+			sys.stdout.write('\r'+'idx=' + str(i))
+			i += 1
+
+			# Make sure the info saved in annotation file matching the format (path_filename, x1, y1, x2, y2, class_name)
+			# Note:
+			#	One path_filename might has several classes (class_name)
+			#	x1, y1, x2, y2 are the pixel value of the origial image, not the ratio value
+			#	(x1, y1) top left coordinates; (x2, y2) bottom right coordinates
+			#   x1,y1-------------------
+			#	|						|
+			#	|						|
+			#	|						|
+			#	|						|
+			#	---------------------x2,y2
+
+			(filename,x1,y1,x2,y2,class_name) = line.strip().split(',')
+
+			if class_name not in classes_count:
+				classes_count[class_name] = 1
+			else:
+				classes_count[class_name] += 1
+
+			if class_name not in class_mapping:
+				if class_name == 'bg' and found_bg == False:
+					print('Found class name with special name bg. Will be treated as a background region (this is usually for hard negative mining).')
+					found_bg = True
+				class_mapping[class_name] = len(class_mapping)
+
+			if filename not in all_imgs:
+				all_imgs[filename] = {}
+				
+				img = cv2.imread(filename)
+				(rows,cols) = img.shape[:2]
+				all_imgs[filename]['filepath'] = filename
+				all_imgs[filename]['width'] = cols
+				all_imgs[filename]['height'] = rows
+				all_imgs[filename]['bboxes'] = []
+				# if np.random.randint(0,6) > 0:
+				# 	all_imgs[filename]['imageset'] = 'trainval'
+				# else:
+				# 	all_imgs[filename]['imageset'] = 'test'
+
+			all_imgs[filename]['bboxes'].append({'class': class_name, 'x1': int(x1), 'x2': int(x2), 'y1': int(y1), 'y2': int(y2)})
+
+
+		all_data = []
+		for key in all_imgs:
+			all_data.append(all_imgs[key])
+		
+		# make sure the bg class is last in the list
+		if found_bg:
+			if class_mapping['bg'] != len(class_mapping) - 1:
+				key_to_switch = [key for key in class_mapping.keys() if class_mapping[key] == len(class_mapping)-1][0]
+				val_to_switch = class_mapping['bg']
+				class_mapping['bg'] = len(class_mapping) - 1
+				class_mapping[key_to_switch] = val_to_switch
+		
+		return all_data, classes_count, class_mapping
+
+
+def get_img_output_length(width, height):
+    def get_output_length(input_length):
+        return input_length//16
+
+    return get_output_length(width), get_output_length(height)    
+	
+
+# UoI
+def union(au, bu, area_intersection):
+	area_a = (au[2] - au[0]) * (au[3] - au[1])
+	area_b = (bu[2] - bu[0]) * (bu[3] - bu[1])
+	area_union = area_a + area_b - area_intersection
+	return area_union
+
+
+def intersection(ai, bi):
+	x = max(ai[0], bi[0])
+	y = max(ai[1], bi[1])
+	w = min(ai[2], bi[2]) - x
+	h = min(ai[3], bi[3]) - y
+	if w < 0 or h < 0:
+		return 0
+	return w*h
+
+
+def iou(a, b):
+	# a and b should be (x1,y1,x2,y2)
+
+	if a[0] >= a[2] or a[1] >= a[3] or b[0] >= b[2] or b[1] >= b[3]:
+		return 0.0
+
+	area_i = intersection(a, b)
+	area_u = union(a, b, area_i)
+
+	return float(area_i) / float(area_u + 1e-6)
